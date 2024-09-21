3d8abe2fce29c0819f17e15e2aad29b0602ebac1
tejas faldu <tejasfaldu02@gmail.com>
2024-09-22 00:03:39 +0530

    add search functionality and update README

--- src/module/products/Products.tsx
+++ src/module/products/Products.tsx
@@ -1,18 +1,37 @@
-import { Box, Button, CircularProgress, Container, Grid2 } from "@mui/material";
+import {
+  Box,
+  Button,
+  CircularProgress,
+  Container,
+  Grid2,
+  IconButton,
+  InputAdornment,
+  TextField,
+  Typography,
+} from "@mui/material";
 import ProductCard from "./components/ProductCard";
 import useProductList from "./hooks/useProductList";
 import { useCallback } from "react";
 import { Item } from "../../model/products.model";
 import { useNavigate } from "react-router-dom";
-import useProductDetails from "./hooks/useProductDetails";
 import { useDispatch } from "react-redux";
 import { setItem } from "../../store/slices/selected-product";
+import SearchIcon from "@mui/icons-material/Search";
 
 const Products = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const { loading, productList, hasNext, hasPrev, onNext, onPrevious } =
-    useProductList();
+  const {
+    loading,
+    productList,
+    hasNext,
+    hasPrev,
+    searchQuery,
+    onChangeSearch,
+    onSearch,
+    onNext,
+    onPrevious,
+  } = useProductList();
 
   const onProductClick = useCallback(
     (item: Item) => {
@@ -28,12 +47,40 @@ const Products = () => {
     </Box>
   ) : (
     <Container>
+      <Box
+        marginBottom={3}
+        display={"flex"}
+        justifyContent={"center"}
+        alignContent={"center"}
+      >
+        <TextField
+          variant="outlined"
+          placeholder="Search..."
+          value={searchQuery}
+          onChange={(e) => onChangeSearch(e.target.value)}
+          slotProps={{
+            input: {
+              endAdornment: (
+                <InputAdornment position="end">
+                  <IconButton onClick={onSearch}>
+                    <SearchIcon />
+                  </IconButton>
+                </InputAdornment>
+              ),
+            },
+          }}
+        />
+      </Box>
       <Grid2 container spacing={3} justifyContent={"center"}>
-        {productList.map((item) => {
-          return (
-            <ProductCard key={item.id} item={item} onClick={onProductClick} />
-          );
-        })}
+        {!!productList.length ? (
+          productList.map((item) => {
+            return (
+              <ProductCard key={item.id} item={item} onClick={onProductClick} />
+            );
+          })
+        ) : (
+          <Typography variant="subtitle1"> Product Not Found</Typography>
+        )}
       </Grid2>
       <Box
         sx={{
--- src/module/products/hooks/useProductList.tsx
+++ src/module/products/hooks/useProductList.tsx
@@ -1,14 +1,15 @@
 import { useQuery } from "@apollo/client";
 import { GET_ALL_ITEMS } from "../../../graphql/queries/product";
-import { useCallback, useMemo } from "react";
+import { useCallback, useMemo, useState } from "react";
 
 const useProductList = () => {
+  const [searchQuery, setSearchQuery] = useState("");
   const { loading, error, data, refetch } = useQuery(GET_ALL_ITEMS, {
     variables: {
       itemPageInput: {
         afterCursor: null,
         beforeCursor: null,
-        limit: 10
+        limit: 10,
       },
     },
   });
@@ -25,23 +26,39 @@ const useProductList = () => {
     return Boolean(data?.paginateItems.cursor.beforeCursor);
   }, [data]);
 
+  const onChangeSearch = useCallback((query: string) => {
+    setSearchQuery(query);
+  }, []);
+
+  const onSearch = useCallback(() => {
+    refetch({
+      itemPageInput: {
+        afterCursor: data?.paginateItems.cursor.afterCursor,
+        beforeCursor: null,
+        name: searchQuery,
+      },
+    });
+  }, [data?.paginateItems.cursor.afterCursor, refetch, searchQuery]);
+
   const onNext = useCallback(() => {
     refetch({
       itemPageInput: {
         afterCursor: data?.paginateItems.cursor.afterCursor,
         beforeCursor: null,
+        name: searchQuery,
       },
     });
-  }, [data?.paginateItems.cursor.afterCursor, refetch]);
+  }, [data?.paginateItems.cursor.afterCursor, refetch, searchQuery]);
 
   const onPrevious = useCallback(() => {
     refetch({
       itemPageInput: {
         afterCursor: null,
         beforeCursor: data?.paginateItems.cursor.beforeCursor,
+        name: searchQuery,
       },
     });
-  }, [data?.paginateItems.cursor.beforeCursor, refetch]);
+  }, [data?.paginateItems.cursor.beforeCursor, refetch, searchQuery]);
 
   return {
     loading,
@@ -49,8 +66,11 @@ const useProductList = () => {
     productList,
     hasNext,
     hasPrev,
+    searchQuery,
+    onChangeSearch,
     onNext,
     onPrevious,
+    onSearch,
   };
 };
 
